6e5d11fb1c54d9562953c7af0d621a8b759af673
neo <grabich@gmail.com>
2013-07-29 12:46:15 +0200

    Updated secret key

--- comp/comp/settings/base.py
+++ comp/comp/settings/base.py
@@ -51,8 +51,20 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'j20*8v$qm6n4%n!3ur0#v0ze&*t-$+ynp(86&h+-4(vw&wr!zl'
+def ensure_secret_key_file():
+    """Checks that secret.py exists in settings dir. If not, creates one
+with a random generated SECRET_KEY setting."""
+    secret_path = os.path.join(ABS_PATH('settings'), 'secret.py')
+    if not os.path.exists(secret_path):
+        from django.utils.crypto import get_random_string
+        secret_key = get_random_string(50,
+                'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)')
+        with open(secret_path, 'w') as f:
+            f.write("SECRET_KEY = " + repr(secret_key) + "\n")
+
+# Import the secret key
+ensure_secret_key_file()
+from secret import SECRET_KEY
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
