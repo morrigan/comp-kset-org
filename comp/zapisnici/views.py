9e33b3eca72f35ee7193d9bc2a975f0555a80dfa
morrigan <belma.dzi@gmail.com>
2013-08-02 13:51:01 -0400

    Created views for 'zapisnici'.

--- comp/zapisnici/views.py
+++ comp/zapisnici/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.http import HttpResponse
+from zapisnici.models import Zapisnik
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.utils import timezone
+
+class IndexView(generic.ListView):
+    template_name = 'zapisnici/index.html'
+
+    def get_queryset(self):
+        return Zapisnik.objects.filter(
+                zap_date__lte=timezone.now()
+                ).order_by('-zap_date')[:5]
+
+class ZapisnikView(generic.DetailView):
+    model = Zapisnik
