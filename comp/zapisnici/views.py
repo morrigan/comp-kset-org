4a36dda92b31cd7517d2d18700b80de0c38cda0f
morrigan <belma.dzi@gmail.com>
2013-08-10 17:02:51 -0400

    Pagination on index page added; 10 on each page.

--- comp/zapisnici/views.py
+++ comp/zapisnici/views.py
@@ -6,9 +6,10 @@ from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'zapisnici/index.html'
+    paginate_by = 10
 
     def get_queryset(self):
-        return Zapisnik.objects.order_by('-zap_date')[:5]
+        return Zapisnik.objects.order_by('-zap_date')
 
 class ZapisnikView(generic.DetailView):
     model = Zapisnik
