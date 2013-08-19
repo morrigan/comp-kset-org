70fc37f396e47565da3739b83d5e324a6f5e42dc
Tomislav Maricevic <tmarice@gmail.com>
2013-08-19 16:41:12 +0200

    Serving media files in development

--- comp/comp/urls.py
+++ comp/comp/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from utils.views import login
 from django.contrib import admin
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,6 +10,11 @@ urlpatterns = patterns('',
     url(r'^tinymce/', include('tinymce.urls')),
     url(r'^login/$', login, name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+            (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
 
 urlpatterns += patterns('django.contrib.flatpages.views',
