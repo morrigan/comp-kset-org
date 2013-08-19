4c9060ff917ac5e5774fb366a321ca0c879cf092
Tomislav Maricevic <tmarice@gmail.com>
2013-08-19 12:35:57 +0200

    Moved decorators to views

--- comp/zapisnici/views.py
+++ comp/zapisnici/views.py
@@ -1,14 +1,21 @@
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from zapisnici.models import Zapisnik
-from django.shortcuts import render, get_object_or_404
-from django.views import generic
-from django.utils import timezone
 
-class IndexView(generic.ListView):
+class IndexView(ListView):
     paginate_by = 10
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(IndexView, self).dispatch(*args, **kwargs)
 
     def get_queryset(self):
         return Zapisnik.objects.order_by('-zap_date')
 
-class ZapisnikView(generic.DetailView):
+class ZapisnikView(DetailView):
     model = Zapisnik
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(DetailView, self).dispatch(*args, **kwargs)
