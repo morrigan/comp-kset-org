0be7fcc56b38e417a955e3dab67d6877ad2301c8
Tomislav Maricevic <tmarice@gmail.com>
2013-08-19 14:55:06 +0200

    Moved view logic to views

--- comp/zapisnici/urls.py
+++ comp/zapisnici/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns, url
-from django.contrib.auth.decorators import login_required
-from zapisnici import views
 
-urlpatterns = patterns('',
-        url(r'^$', views.IndexView.as_view()), # /zapisnici/
-        url(r'^(?P<pk>\d+)/$', views.ZapisnikView.as_view(), name='zapisnik'), # /zapisnici/3
+urlpatterns = patterns('zapisnici.views',
+        url(r'^$', 'index'), # /zapisnici/
+        url(r'^(?P<pk>\d+)/$', 'detail', name='zapisnik'), # /zapisnici/3
 )
