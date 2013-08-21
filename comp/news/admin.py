36027b5238f210fdf38e636e10db5765ae8425cb
Tomislav Maricevic <tmarice@gmail.com>
2013-08-21 16:12:27 +0200

    Added auto-population for author field

--- comp/news/admin.py
+++ comp/news/admin.py
@@ -5,18 +5,18 @@ from tinymce.widgets import TinyMCE
 from django.conf import settings
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'body', 'date')
-    fieldsets = [
-        (None, { 'fields': [('title','date','body')] } ),
-    ]
+    list_display = ('title', 'author','body', 'date')
+    fieldsets = (
+        (None, { 'fields': ['title', 'body']} ),
+    )
 
     def save_model(self, request, obj, form, change):
-        if getattr(obj, 'author', None) in None:
+        if not change:
             obj.author = request.user
-        obj.save()
+        super(NewsAdmin, self).save_model(request, obj, form, change)
 
     formfield_overrides = {
         models.TextField: {'widget': TinyMCE(attrs={'cols': 80, 'rows': 30})},
     }
 
-admin.site.register(News)
+admin.site.register(News, NewsAdmin)
