52bade2d56149f7cec7bf28d087bc378a957cabb
Tomislav Maricevic <tmarice@gmail.com>
2013-08-21 16:14:44 +0200

    Moved logic to views

--- comp/news/urls.py
+++ comp/news/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
-from news import views
 
-urlpatterns = patterns('',
-        url(r'^$', login_required(views.IndexView.as_view()), name='index'), 
-        url(r'^(?P<pk>\d+)/$', login_required(views.NewsView.as_view()), name='news'), 
+urlpatterns = patterns('news.views',
+        url(r'^$', 'index', name='index'), 
+        url(r'^(?P<pk>\d+)/$', 'detail', name='news'), 
 )
