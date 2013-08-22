347324d229e2490304d91ec46d4c1c843266ddfd
morrigan <belma.dzi@gmail.com>
2013-08-22 01:17:50 -0400

    Email-Notify code moved from admin to models.py

--- comp/zapisnici/admin.py
+++ comp/zapisnici/admin.py
@@ -4,34 +4,9 @@ from zapisnici.models import Zapisnik
 from django.db import models
 from tinymce.widgets import TinyMCE
 
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import get_template
-from django.template import Context
-from django.conf import settings
-
 class ZapisnikAdmin(admin.ModelAdmin):
-    fields = ['zap_date', 'zap_sadrzaj', 'prisutni', 'mail_notifikacija']
-    list_display = ('zap_date', 'zap_sadrzaj', 'mail_notifikacija')
-
-    def save_model(self, request, obj, form, change):
-        if obj.mail_notifikacija == True:
-
-            plaintext = get_template('email.txt')
-            htmltext = get_template('email.html')
-
-            d = Context({ 'datum' : obj.zap_date,
-                'prisutni' : obj.prisutni,
-                'sadrzaj' : obj.zap_sadrzaj })
-
-            subject, from_email, to = 'Zapisnik sa sastanka', settings.EMAIL_HOST_USER, 'sendto@mail.com'
-            text_content = plaintext.render(d)
-            html_content = htmltext.render(d)
-            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-            msg.attach_alternative(html_content, "text/html")
-            msg.send()
-
-        super(ZapisnikAdmin, self).save_model(request, obj, form, change)
-
+    fields = ['zap_date', 'zap_sadrzaj', 'prisutni','mail_notifikacija']
+    list_display = ('zap_date', 'zap_sadrzaj','mail_notifikacija')
 
     formfield_overrides = {
             models.TextField: {'widget': TinyMCE(attrs={'cols': 80, 'rows': 30})},
--- comp/zapisnici/models.py
+++ comp/zapisnici/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from tinymce.widgets import TinyMCE
 
+from django.template.loader import get_template
+from django.template import Context
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+
 class Zapisnik(models.Model):
     zap_date = models.DateTimeField('datum zapisnika')
     zap_sadrzaj = models.TextField('sadrzaj zapisnika')
@@ -8,8 +13,27 @@ class Zapisnik(models.Model):
     mail_notifikacija = models.BooleanField(default=False)
 
     def __unicode__(self):
-        return self.zap_sadrzaj # Promijeniti u nesto pametnije
+        return self.zap_sadrzaj
 
     class Meta:
         verbose_name_plural = "zapisnici"
 
+    def save(self):
+        if self.mail_notifikacija == True:
+
+            plaintext = get_template('email.txt')
+            htmltext = get_template('email.html')
+
+            d = Context({ 'datum' : self.zap_date,
+                'prisutni' : self.prisutni,
+                'sadrzaj' : self.zap_sadrzaj })
+
+            subject, from_email, to = 'Zapisnik sa sastanka', settings.EMAIL_HOST_USER, 'morrigan@kset.org'
+            text_content = plaintext.render(d)
+            html_content = htmltext.render(d)
+            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+
+            super(Zapisnik, self).save()
+   
